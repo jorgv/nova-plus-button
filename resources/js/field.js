0d6f4614aabf7add017c1974b30c35697e3d69d4
Jorge Villafuerte <jorge@avantpage.com>
2018-08-27 23:36:50 +0200

    adding toastr notification

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,4 +1,10 @@
+import Toasted from 'vue-toasted'
 Nova.booting((Vue, router) => {
+    Vue.use(Toasted, {
+        theme: 'nova',
+        position: 'bottom-right',
+        duration: 6000,
+    })
     Vue.component('index-plus-button', require('./components/IndexField'));
     Vue.component('detail-plus-button', require('./components/DetailField'));
     Vue.component('form-plus-button', require('./components/FormField'));
