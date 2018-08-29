674daaceb910ee0daad92479e8b4372d072a45ab
Jorge Villafuerte <jorge@avantpage.com>
2018-08-29 23:07:25 +0200

    Adding buttons in Form view

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -2,7 +2,7 @@
     <default-field :field="field">
         <template slot="field">
             <input :id="field.name" type="text"
-                class="w-full form-control form-input form-input-bordered"
+                class="w-full form-control form-input form-input-bordered disabled"
                 :class="errorClasses"
                 :placeholder="field.name"
                 v-model="value"
@@ -12,6 +12,20 @@
                 {{ firstError }}
             </p>
         </template>
+        <button
+        @click="Increase"
+        class="cursor-pointer text-70 hover:text-primary mr-3"
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" aria-labelledby="updateRequest" role="presentation" class="fill-current">
+            <path d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1-9h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 0v-2H9a1 1 0 0 1 0-2h2V9a1 1 0 0 1 2 0v2z"/></svg>
+        </button>
+        <button
+        @click="Decrease"
+        class="cursor-pointer text-70 hover:text-primary mr-3" v-show="field.decrease"
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" aria-labelledby="updateRequest" role="presentation" class="fill-current">
+            <path d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm4-8a1 1 0 0 1-1 1H9a1 1 0 0 1 0-2h6a1 1 0 0 1 1 1z"/></svg>
+        </button>     
     </default-field>
 </template>
 
@@ -43,7 +57,19 @@ export default {
          */
         handleChange(value) {
           this.value = value
-        }
+        },
+        /**
+         * Send an update request to increase value for this resource
+         */
+        Increase() {
+            this.value += this.field.increaseValue;          
+        },
+        /**
+         * Send an update request to decrease value for this resource
+         */
+        Decrease() {
+            this.value -= this.field.increaseValue;          
+        },        
     }
 }
 </script>
--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -7,14 +7,13 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" aria-labelledby="updateRequest" role="presentation" class="fill-current">
             <path d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1-9h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 0v-2H9a1 1 0 0 1 0-2h2V9a1 1 0 0 1 2 0v2z"/></svg>
         </button>
-        <span>{{field.value}}</span>
+        <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">{{field.value}}</span>
         <button
         @click="Decrease"
         class="cursor-pointer text-70 hover:text-primary mr-3" v-show="field.decrease"
         >
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" aria-labelledby="updateRequest" role="presentation" class="fill-current">
             <path d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm4-8a1 1 0 0 1-1 1H9a1 1 0 0 1 0-2h6a1 1 0 0 1 1 1z"/></svg>
-            <span>{{field.value}}</span>
         </button>        
     </span>
 </template>
